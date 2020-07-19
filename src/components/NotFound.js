ba227800e3a3c53816d77639e05f5ef8b0a86e4c
David Gabrielian <davidgabrielofficial@gamil.com>
2020-07-19 18:00:49 +0400

    '5.7.4'

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,11 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class NotFound extends Component {
-    render() {
-        return (
-            <div>
-                <h1>page not found</h1>
-            </div>
-        )
-    }
+  render() {
+    console.log(this.props);
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
+            <h1 className="display-3"> 404</h1>
+            <h1>error</h1>
+            <br />
+            <h2>poage not found</h2>
+            <br/>
+            <br/>
+            <br/>
+            <h3>
+              the requested URL
+              <span className="text-danger">
+                {this.props.location.pathname}
+              </span>{" "}
+              is invalid
+            </h3>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
