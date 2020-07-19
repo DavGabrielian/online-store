1fb79694a17aa78a5ba35f322a5ba3cf0a26796b
David Gabrielian <davidgabrielofficial@gamil.com>
2020-07-19 18:19:51 +0400

    Updates

---
+++ precache-manifest.493976edcc9b82541c6f1f0452c1d300.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0ddbd66947de17b2838e0f7277758af",
+    "url": "/online-store/index.html"
+  },
+  {
+    "revision": "0df4f704bedb78bb096a",
+    "url": "/online-store/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a700831baec0daa9519a",
+    "url": "/online-store/static/css/main.79d5ad6f.chunk.css"
+  },
+  {
+    "revision": "0df4f704bedb78bb096a",
+    "url": "/online-store/static/js/2.4a10e139.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/online-store/static/js/2.4a10e139.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a700831baec0daa9519a",
+    "url": "/online-store/static/js/main.4d3d893c.chunk.js"
+  },
+  {
+    "revision": "e990af296427bd6ef7db",
+    "url": "/online-store/static/js/runtime-main.12f6060e.js"
+  },
+  {
+    "revision": "906750726d6278d7f77666948e9647f6",
+    "url": "/online-store/static/media/back.90675072.png"
+  },
+  {
+    "revision": "4d0d1f06bb61ba4ebaaaebd5903b08b6",
+    "url": "/online-store/static/media/home.4d0d1f06.png"
+  },
+  {
+    "revision": "9a5f45364ebee21aaf64d2b3fc3565bb",
+    "url": "/online-store/static/media/home0.9a5f4536.png"
+  },
+  {
+    "revision": "481e3c4837de0407ed7e4c236e142fc6",
+    "url": "/online-store/static/media/logo.481e3c48.svg"
+  },
+  {
+    "revision": "d63d9eff35aa057bab9c5041938d6d69",
+    "url": "/online-store/static/media/phone-1.d63d9eff.png"
+  },
+  {
+    "revision": "ad7ef891046b2a56aa41db25cbcf2a9b",
+    "url": "/online-store/static/media/phone-2.ad7ef891.png"
+  },
+  {
+    "revision": "d27ec2bc66073aa4b0b80368a9ac410e",
+    "url": "/online-store/static/media/phone-3.d27ec2bc.png"
+  },
+  {
+    "revision": "d9ed75372e686c2f8c38a53a52482e66",
+    "url": "/online-store/static/media/phone-4.d9ed7537.png"
+  },
+  {
+    "revision": "8f9a1f89508713ff90dc1b497c92b1b2",
+    "url": "/online-store/static/media/phone-5.8f9a1f89.png"
+  },
+  {
+    "revision": "b49d1b7cf1b82dad0a8059302706685a",
+    "url": "/online-store/static/media/phone-6.b49d1b7c.png"
+  },
+  {
+    "revision": "4c6ab223c32bec22d9cd6070caaedf19",
+    "url": "/online-store/static/media/phone-7.4c6ab223.png"
+  },
+  {
+    "revision": "d390282f89018d8777d36210b7df6d7b",
+    "url": "/online-store/static/media/phone-8.d390282f.png"
+  },
+  {
+    "revision": "0a73330f0755733d404e50976627bb59",
+    "url": "/online-store/static/media/tablet-10.0a73330f.png"
+  },
+  {
+    "revision": "2ab318e64ad43ebeb5f01ecf5ceb860f",
+    "url": "/online-store/static/media/tablet-11.2ab318e6.png"
+  },
+  {
+    "revision": "92102174a031ce195fe11bea33ebe13c",
+    "url": "/online-store/static/media/tablet-12.92102174.png"
+  },
+  {
+    "revision": "e2f92865729906b3c004ab57f21fc1e2",
+    "url": "/online-store/static/media/tablet-13.e2f92865.png"
+  },
+  {
+    "revision": "b673d22704e1991f3aadecb0bd0144d1",
+    "url": "/online-store/static/media/tablet-14.b673d227.png"
+  },
+  {
+    "revision": "31edb278feee330f47f93c5a7e2c55a4",
+    "url": "/online-store/static/media/tablet-15.31edb278.png"
+  },
+  {
+    "revision": "5fb3397a446574223bf50aa8d170d173",
+    "url": "/online-store/static/media/tablet-16.5fb3397a.png"
+  },
+  {
+    "revision": "7fccd70b7149f29bab3c0dd356aebc1f",
+    "url": "/online-store/static/media/tablet-9.7fccd70b.png"
+  },
+  {
+    "revision": "b00714a7432d41049237d69ddb8c4b5b",
+    "url": "/online-store/static/media/watch-17.b00714a7.png"
+  },
+  {
+    "revision": "9b8e286d06b74af80f8cd3b9ec350c39",
+    "url": "/online-store/static/media/watch-18.9b8e286d.png"
+  },
+  {
+    "revision": "7def4dc77fb7c1fadc0f0e576bd1a9cc",
+    "url": "/online-store/static/media/watch-19.7def4dc7.png"
+  },
+  {
+    "revision": "f7c8f220e0cba9c825b8e551652d6052",
+    "url": "/online-store/static/media/watch-20.f7c8f220.png"
+  },
+  {
+    "revision": "6248e169f2956edb0473d888d1785860",
+    "url": "/online-store/static/media/watch-21.6248e169.png"
+  },
+  {
+    "revision": "139657e6cf4b46208363ed8f89b0ecf3",
+    "url": "/online-store/static/media/watch-22.139657e6.png"
+  },
+  {
+    "revision": "aa2c223c4285f74241f41e80e7251849",
+    "url": "/online-store/static/media/watch-23.aa2c223c.png"
+  },
+  {
+    "revision": "ec506f6b9457830b28832f245ab54e61",
+    "url": "/online-store/static/media/watch-24.ec506f6b.png"
+  }
+]);
